1262b59e5400f431140061e4bf44523e2b8a1857
Dorian <dorianbirraux@gmail.com>
2014-03-21 16:42:17 +0100

    AppendString build resizable array of string

--- ArrayTools.go
+++ ArrayTools.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 )
 
+// And array of int that auto increase its capacity when needed
 func AppendInt(slice []int, elements ...int) []int {
 	ori_size := len(slice)
 	final_size := len(slice) + len(elements)
@@ -14,6 +15,25 @@ func AppendInt(slice []int, elements ...int) []int {
 	}
 	if cap(slice) < final_size {
 		newSlice := make([]int, len(slice)+len(elements), total)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[:final_size]
+	copy(slice[ori_size:], elements)
+	return slice
+}
+
+// And array of int that auto increase its capacity when needed
+func AppendString(slice []string, elements ...string) []string {
+	ori_size := len(slice)
+	final_size := len(slice) + len(elements)
+	total := cap(slice)
+	// We double the size as long as the size is not big enough
+	for total < final_size {
+		total = 2*total + 1
+	}
+	if cap(slice) < final_size {
+		newSlice := make([]string, len(slice)+len(elements), total)
 		copy(newSlice, slice)
 		slice = newSlice
 	}
--- arrayTools_test.go
+++ arrayTools_test.go
@@ -10,7 +10,7 @@ func TestInitArrayTools(t *testing.T) {
 	Logger.Init()
 
 }
-func TestAppend(t *testing.T) {
+func TestAppendInt(t *testing.T) {
 	t.SkipNow()
 	a := make([]int, 5, 6)
 	b := make([]int, 10)
@@ -38,6 +38,28 @@ func TestShuffleInt(i *testing.T) {
 	Logger.Debugf("Original : %v", a)
 	ShuffleInt(a)
 	Logger.Debugf("Shuffled : %v", a)
+}
+func TestAppendStr(t *testing.T) {
+
+	a := make([]string, 5, 6)
+	b := make([]string, 10)
+	for i := 0; i < len(a); i++ {
+		a[i] = "Elem" + strconv.Itoa(i)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = "Elem" + strconv.Itoa(10+i)
+	}
+	Logger.Debugf("a=%v , len=%v , cap=%v", a, len(a), cap(a))
+	Logger.Debugf("b=%v , len=%v , cap=%v", b, len(b), cap(b))
+	a = AppendString(a, "New value1")
+	Logger.Debugf("%v , len=%v , cap=%v", a, len(a), cap(a))
+	a = AppendString(a, "New value2")
+	Logger.Debugf("%v , len=%v , cap=%v", a, len(a), cap(a))
+	a = AppendString(a, b...)
+	Logger.Debugf("%v , len=%v , cap=%v", a, len(a), cap(a))
+	if cap(a) != 27 || len(a) != 17 {
+		t.Errorf("Unexpected format for the array. Found len=%v, cap=%v. Expected len=%v, cap=%v.", len(a), cap(a), 17, 27)
+	}
 }
 func TestShuffleString(i *testing.T) {
 	size := 10
